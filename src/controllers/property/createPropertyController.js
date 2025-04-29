4c1fc1561051afdf16438883070d4aec284ea467
KauanLeonel <kauan.br.char@gmail.com>
2025-04-29 20:38:08 -0300

    Aplicando o conceito de middleware e colocando um logger no acesso do server

--- src/controllers/property/createPropertyController.js
+++ src/controllers/property/createPropertyController.js
@@ -1,10 +1,24 @@
-import { create } from "../../models/propertyModel.js";
+import { create, propertyValidator } from "../../models/propertyModel.js";
 export default async function createPropertyController(req, res) {
 
     const property = req.body;
 
-    //TODO validar
-    const result = await create(property)
+    // Desestrutura o resultado da função propertyValidator:
+    // - 'success': indica se a validação foi bem-sucedida
+    // - 'error': contém os erros, se houver
+    // - 'data' é renomeado para 'propertyValited' e contém os dados validados (caso válidos)
+    const { success, error, data: propertyValited } = propertyValidator(property, { id: true })
+
+    // Verifica se a validação falhou (success é false)
+    if (!success) {
+        // Retorna uma resposta HTTP 400 (Bad Request) com uma mensagem de erro e os erros detalhados por campo
+        return res.status(400).json({
+            message: 'Erro ao cadastrar a propriedade', // Mensagem geral do erro
+            errors: error.flatten().fieldErrors          // Lista de erros por campo (formato do Zod)
+        })
+    }
+
+    const result = await create(propertyValited)
 
 
     return res.json({
--- src/models/propertyModel.js
+++ src/models/propertyModel.js
@@ -1,35 +1,51 @@
 import { PrismaClient } from "@prisma/client";
+import { string, z } from 'zod'; //Importar o zod
 
 const prisma = new PrismaClient()
 
+const propertySchema = z.object({//Cria o objeto para as validações
+    id:z.number().positive(),
+    type: z.string().min(5).max(7),
+    endress: string().min(6).max(500),
+    rooms: number().positive(),
+    property: string().min(4).max(11)
+}) 
+
+export const propertyValidator = (property, partial = null) => {
+    if(partial){
+        return propertySchema.partial({partial}).safeParse(property);
+    }
+    return propertySchema.safeParse(property)
+}
+
 //prisma.users.findMany() //Buscar todos os itens da tabela
 
-export async function create(property){
+export async function create(property) {
     const result = await prisma.property.create({
         data: property
     })
     return result
 }
 
-export async function remove(id){
+export async function remove(id) {
     const result = await prisma.property.delete({
-        where:{ //Quando o id for igual
+        where: { //Quando o id for igual
             id //Mesma coisa que falar id : id
         }
     })
     return result
 }
 
-export async function getList(){
+export async function getList() {
     const result = await prisma.property.findMany({
-        
+
     })
     return result
 }
 
-export async function update(id, property){
+export async function update(id, property) {
     const result = await prisma.property.update({
-        where:{ //Quando o id for igual
+        where: { //Quando o id for igual
             id //Mesma coisa que falar id : id
         },
         data: property
